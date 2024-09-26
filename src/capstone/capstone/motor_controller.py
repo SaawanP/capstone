16d63ff7fbb6bbccfa8d9573500d198284344004
SaawanP <saawan.patel@uwaterloo.ca>
2024-09-26 17:19:13 -0400

    add GPIO

--- src/capstone/capstone/motor_controller.py
+++ src/capstone/capstone/motor_controller.py
@@ -5,10 +5,13 @@ from rclpy.node import Node
 
 from robot_interface.msg import Speed
 
+import RPi.GPIO as GPIO
+
 
 class MotorController(Node):
     def __init__(self):
         super().__init__('motor_controller')
+        # TODO move parameters to ROS param
         self.MAX_RPM = 70  # rpm TODO fix numbers
         self.CIRC = 1  # m
         self.MAX_SPEED = self.MAX_RPM * self.CIRC  # m/min
@@ -16,6 +19,26 @@ class MotorController(Node):
         self.Kp = 0.8  # TODO tune parameters
         self.Ki = 0.2
         self.Kd = 0.1
+
+        # TODO fix pin numbers
+        self.M1_in1 = 9
+        self.M1_in2 = 10
+        self.M1_en = 11
+        self.M2_in1 = 12
+        self.M2_in2 = 13
+        self.M2_en = 14
+
+        # Pin setup
+        GPIO.setup(self.M1_in1, GPIO.OUT)
+        GPIO.setup(self.M1_in2, GPIO.OUT)
+        GPIO.setup(self.M2_in1, GPIO.OUT)
+        GPIO.setup(self.M2_in2, GPIO.OUT)
+        GPIO.setup(self.M1_en, GPIO.OUT)
+        GPIO.setup(self.M2_en, GPIO.OUT)
+        self.M1_pwm = GPIO.pwm(self.M1_en, 1000)  # TODO change frequency
+        self.M2_pwm = GPIO.pwm(self.M2_en, 1000)
+        self.M1_pwm.start(0)
+        self.M2_pwm.start(0)
 
         # Subscribers and publishers
         self.speed_sub = self.create_subscription(
@@ -38,11 +61,16 @@ class MotorController(Node):
     def speed_callback(self, msg):
         speed = msg.dist * self.MAX_SPEED
         if msg.x / abs(msg.x) > 0:
-            # TODO high on Pin1 for both motors and low on Pin 2
-            pass
+            GPIO.output(self.M1_in1, GPIO.HIGH)
+            GPIO.output(self.M2_in1, GPIO.HIGH)
+            GPIO.output(self.M1_in2, GPIO.LOW)
+            GPIO.output(self.M2_in2, GPIO.LOW)
         else:
-            # TODO high on Pin2 for both motors and low on Pin 1
-            pass
+            GPIO.output(self.M1_in1, GPIO.LOW)
+            GPIO.output(self.M2_in1, GPIO.LOW)
+            GPIO.output(self.M1_in2, GPIO.HIGH)
+            GPIO.output(self.M2_in2, GPIO.HIGH)
+
         if msg.y == 0:
             self.left_rpm = self.right_rpm = speed / self.CIRC
             return
@@ -62,6 +90,7 @@ class MotorController(Node):
             f"right rpm: {self.right_rpm}, left rpm: {self.left_rpm}, direction: {msg.x / abs(msg.x)}")
 
     def PID_controller(self):
+        #https://github.com/rakesh-i/MicroPython-Encoder-motor/blob/main/encoder_N20_esp.py
         # TODO get current rpm
         left_measurement = 0
         right_measurement = 0
@@ -80,24 +109,24 @@ class MotorController(Node):
         D = self.Kd * (e - self.right_e_prev) / self.timer_period
         right_rpm = P + self.integral + D
 
-        left_pwm = self.pwm_map(left_rpm)
-        right_pwm = self.pwm_map(right_rpm)
+        self.M1_pwm.ChangeDutyCycle(self.duty_cycle_map(left_rpm))
+        self.M2_pwm.ChangeDutyCycle(self.duty_cycle_map(right_rpm))
 
-        # TODO set pwm for motors
-
-    def pwm_map(self, x):
+    def duty_cycle_map(self, x):
         # TODO check if rpm is a linear function to rpm
-        if 0 < x < self.MAX_RPM:
+        if not (0 < x < self.MAX_RPM):
             self.get_logger().info(f"rpm is {x} which is out of range")
-        return x / self.MAX_RPM * 255
+        return x / self.MAX_RPM
 
 
 def main(args=None):
+    GPIO.setmode(GPIO.BOARD)
     rclpy.init(args=args)
     motor_controller = MotorController()
     rclpy.spin(motor_controller)
     motor_controller.destroy_node()
     rclpy.shutdown()
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
