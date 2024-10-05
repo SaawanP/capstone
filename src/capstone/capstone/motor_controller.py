292f341d15fb47893598049ed7f19af1b0254ec8
SaawanP <saawan.patel@uwaterloo.ca>
2024-10-05 17:25:17 -0400

    encoder and PID controller implemented

--- src/capstone/capstone/motor_controller.py
+++ src/capstone/capstone/motor_controller.py
@@ -3,10 +3,10 @@
 import rclpy
 from rclpy.node import Node
 
-from robot_interface.msg import Speed
-
 import RPi.GPIO as GPIO
 
+from robot_interface.msg import Speed
+from motor import Motor, PID
 
 class MotorController(Node):
     def __init__(self):
@@ -20,25 +20,18 @@ class MotorController(Node):
         self.Ki = 0.2
         self.Kd = 0.1
 
-        # TODO fix pin numbers
-        self.M1_in1 = 9
-        self.M1_in2 = 10
-        self.M1_en = 11
-        self.M2_in1 = 12
-        self.M2_in2 = 13
-        self.M2_en = 14
+        # Motor setup
+        self.M1 = Motor(9, 10, 11, 12, 13, self.MAX_SPEED)
+        self.M2 = Motor(14,15, 16, 17,18, self.MAX_SPEED)
+        self.left_rpm = 0
+        self.right_rpm = 0
 
-        # Pin setup
-        GPIO.setup(self.M1_in1, GPIO.OUT)
-        GPIO.setup(self.M1_in2, GPIO.OUT)
-        GPIO.setup(self.M2_in1, GPIO.OUT)
-        GPIO.setup(self.M2_in2, GPIO.OUT)
-        GPIO.setup(self.M1_en, GPIO.OUT)
-        GPIO.setup(self.M2_en, GPIO.OUT)
-        self.M1_pwm = GPIO.pwm(self.M1_en, 1000)  # TODO change frequency
-        self.M2_pwm = GPIO.pwm(self.M2_en, 1000)
-        self.M1_pwm.start(0)
-        self.M2_pwm.start(0)
+
+        # PID setup
+        timer_period = 0.1  # s TODO find best period
+        self.PID1 = PID(self.M1, timer_period, kp=self.Kp, kd=self.Kd, ki=self.Ki)
+        self.PID2 = PID(self.M2, timer_period, kp=self.Kp, kd=self.Kd, ki=self.Ki)
+        self.timer = self.create_timer(timer_period, self.PID_controller)
 
         # Subscribers and publishers
         self.speed_sub = self.create_subscription(
@@ -48,28 +41,8 @@ class MotorController(Node):
             10
         )
 
-        self.timer_period = 0.5  # s
-        self.timer = self.create_timer(self.timer_period, self.PID_controller)  # change period
-        self.left_integral = 0
-        self.right_integral = 0
-        self.left_e_prev = 0
-        self.right_e_prev = 0
-
-        self.left_rpm = 0
-        self.right_rpm = 0
-
     def speed_callback(self, msg):
         speed = msg.dist * self.MAX_SPEED
-        if msg.x / abs(msg.x) > 0:
-            GPIO.output(self.M1_in1, GPIO.HIGH)
-            GPIO.output(self.M2_in1, GPIO.HIGH)
-            GPIO.output(self.M1_in2, GPIO.LOW)
-            GPIO.output(self.M2_in2, GPIO.LOW)
-        else:
-            GPIO.output(self.M1_in1, GPIO.LOW)
-            GPIO.output(self.M2_in1, GPIO.LOW)
-            GPIO.output(self.M1_in2, GPIO.HIGH)
-            GPIO.output(self.M2_in2, GPIO.HIGH)
 
         if msg.y == 0:
             self.left_rpm = self.right_rpm = speed / self.CIRC
@@ -77,7 +50,7 @@ class MotorController(Node):
 
         r = abs(1 / msg.y)  # cm
         w1 = r / (r + self.WIDTH / 2) * speed / self.CIRC
-        w2 = (r + self.width) / (r + self.WIDTH / 2) * speed / self.CIRC
+        w2 = (r + self.WIDTH) / (r + self.WIDTH / 2) * speed / self.CIRC
 
         if msg.y > 0:
             self.right_rpm = w1
@@ -90,43 +63,20 @@ class MotorController(Node):
             f"right rpm: {self.right_rpm}, left rpm: {self.left_rpm}, direction: {msg.x / abs(msg.x)}")
 
     def PID_controller(self):
-        #https://github.com/rakesh-i/MicroPython-Encoder-motor/blob/main/encoder_N20_esp.py
-        # TODO get current rpm
-        left_measurement = 0
-        right_measurement = 0
-
-        # left PID controller
-        e = self.left_rpm - left_measurement
-        P = self.Kp * e
-        self.left_integral += self.Ki * e * self.timer_period
-        D = self.Kd * (e - self.left_e_prev) / self.timer_period
-        left_rpm = P + self.integral + D
-
-        # right PID controller
-        e = self.right_rpm - right_measurement
-        P = self.Kp * e
-        self.right_integral += self.Ki * e * self.timer_period
-        D = self.Kd * (e - self.right_e_prev) / self.timer_period
-        right_rpm = P + self.integral + D
-
-        self.M1_pwm.ChangeDutyCycle(self.duty_cycle_map(left_rpm))
-        self.M2_pwm.ChangeDutyCycle(self.duty_cycle_map(right_rpm))
-
-    def duty_cycle_map(self, x):
-        # TODO check if rpm is a linear function to rpm
-        if not (0 < x < self.MAX_RPM):
-            self.get_logger().info(f"rpm is {x} which is out of range")
-        return x / self.MAX_RPM
+        self.PID1.set_target_speed(self.left_rpm)
+        self.PID2.set_target_speed(self.right_rpm)
 
 
 def main(args=None):
-    GPIO.setmode(GPIO.BOARD)
-    rclpy.init(args=args)
     motor_controller = MotorController()
-    rclpy.spin(motor_controller)
-    motor_controller.destroy_node()
-    rclpy.shutdown()
-    GPIO.cleanup()
+    try:
+        GPIO.setmode(GPIO.BOARD)
+        rclpy.init(args=args)
+        rclpy.spin(motor_controller)
+    finally:
+        motor_controller.destroy_node()
+        rclpy.shutdown()
+        GPIO.cleanup()
 
 
 if __name__ == '__main__':
