56d77efbad305efc4f7ba986334671a86f35174e
SaawanP <s463pate@uwaterloo.ca>
2024-10-09 11:58:27 -0400

    add speed monitor

--- src/capstone/capstone/motor.py
+++ src/capstone/capstone/motor.py
@@ -1,15 +1,23 @@
 import RPi.GPIO as GPIO
+import time
+import csv
+from typing import Optional
 
 
 class Motor:
-    def __init__(self, in1, in2, en, enc_A, enc_B, max_speed):
+    def __init__(self, in1, in2, en, enc_A, enc_B, max_speed, encoder_ticks=350, tracking_name: Optional[str] = None):
+        self.file_name = "data/" + tracking_name if tracking_name else None
+        self.max_speed = max_speed
+        self.encoder_ticks = encoder_ticks
         self.in1 = in1
         self.in2 = in2
         self.en = en
         self.enc_A = enc_A
         self.enc_B = enc_B
         self.pos = 0
-        self.max_speed = max_speed
+        self.last_pos = 0
+        self.last_event_time = time.time()
+        self.speed = 0
 
         # Pin setup
         GPIO.setup(self.in1, GPIO.OUT)
@@ -22,13 +30,35 @@ class Motor:
         GPIO.setup(self.enc_B, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(self.enc_A, GPIO.RISING, callback=self.handle_event)
 
+        if self.file_name:
+            self.fieldnames = ["Time", "Speed"]
+            with open(self.file_name, 'w') as csv_file:
+                csv_writer = csv.DictWriter(csv_file, fieldnames=self.fieldnames)
+                csv_writer.writeheader()
+
     def handle_event(self, pin):
         if GPIO.input(self.enc_B):
             self.pos = self.pos + 1
         else:
             self.pos = self.pos - 1
 
-    def convertToDuty(self, x):
+        dt = time.time() - self.last_event_time
+        self.last_event_time = time.time()
+        dx = self.pos - self.last_pos
+        self.last_pos = self.pos
+        v = dx / dt  # ticks/sec
+        self.speed = v / self.encoder_ticks * 60  # RPM
+
+        if self.file_name:
+            with open(self.file_name, 'a') as csv_file:
+                csv_writer = csv.DictWriter(csv_file, fieldnames=self.fieldnames)
+                info = {
+                    "Time": time.time(),
+                    "Speed": self.speed
+                }
+                csv_writer.writerow(info)
+
+    def convert_to_duty(self, x):
         if x > self.max_speed:
             ValueError(f"Needed speed of {x} is higher than max speed of {self.max_speed}")
         elif x < 0:
@@ -37,7 +67,7 @@ class Motor:
         return (x * 100) // self.max_speed
 
     def set_speed(self, speed):
-        duty = self.convertToDuty(abs(speed))
+        duty = self.convert_to_duty(abs(speed))
         self.pwm.ChangeDutyCycle(duty)
         if speed > 0:
             GPIO.output(self.in1, 1)
@@ -48,7 +78,8 @@ class Motor:
 
 
 class PID:
-    def __init__(self, M: Motor, period, kp=1, kd=0, ki=0, e_prev=0, e_integral=0):
+    def __init__(self, M: Motor, period, kp=1, kd=0, ki=0, e_prev=0, e_integral=0, tracking_name: Optional[str] = None):
+        self.file_name = "data/" + tracking_name if tracking_name else None
         self.M: Motor = M  # Motor object
         self.period = period
         self.kp = kp
@@ -57,10 +88,16 @@ class PID:
         self.e_prev = e_prev
         self.e_integral = e_integral
         self.posPrev = 0
+        self.calculated_speed = 0
+
+        if self.file_name:
+            self.fieldnames = ["Time", "Speed", "Control Signal"]
+            with open(self.file_name, 'w') as csv_file:
+                csv_writer = csv.DictWriter(csv_file, fieldnames=self.fieldnames)
+                csv_writer.writeheader()
 
     def eval(self, value, target, deltaT):
-
-        # Propotional
+        # Proportional
         e = target - value
 
         # Derivative
@@ -87,8 +124,7 @@ class PID:
         self.posPrev = pos
 
         # Converted to RPM
-        # 350 ticks per revolution of output shaft of the motor
-        v = velocity / 350 * 60
+        v = velocity / self.M.encoder_ticks * 60
 
         # Call for control signal
         x = int(self.eval(v, vt, self.period))
@@ -96,7 +132,15 @@ class PID:
         # Bound control signal
         x = min(max(x, -self.M.max_speed), self.M.max_speed)
 
+        if self.file_name:
+            with open(self.file_name, 'a') as csv_file:
+                csv_writer = csv.DictWriter(csv_file, fieldnames=self.fieldnames)
+                info = {
+                    "Time": time.time(),
+                    "Speed": v,
+                    "Control Signal": x
+                }
+                csv_writer.writerow(info)
+
         # Set the motor speed
         self.M.set_speed(x)
-        # print(v, vt)  # For debugging
-
--- src/capstone/capstone/motor_controller.py
+++ src/capstone/capstone/motor_controller.py
@@ -5,8 +5,42 @@ from rclpy.node import Node
 
 import RPi.GPIO as GPIO
 
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import pandas as pd
+
 from robot_interface.msg import Speed
 from motor import Motor, PID
+
+
+def speed_monitor():
+    m1_data = pd.read_csv('data/M1.csv')
+    m1_time = m1_data["Time"]
+    m1_speed = m1_data["Speed"]
+
+    m2_data = pd.read_csv('data/M2.csv')
+    m2_time = m2_data["Time"]
+    m2_speed = m2_data["Speed"]
+
+    pid1_data = pd.read_csv('data/PID1.csv')
+    pid1_time = pid1_data["Time"]
+    pid1_speed = pid1_data["Speed"]
+    pid1_control = pid1_data["Control Signal"]
+
+    pid2_data = pd.read_csv('data/PID2.csv')
+    pid2_time = pid2_data["Time"]
+    pid2_speed = pid2_data["Speed"]
+    pid2_control = pid2_data["Control Signal"]
+
+    plt.cla()
+    plt.plot(m1_time, m1_speed, label="M1 Speed")
+    plt.plot(m2_time, m2_speed, label="M2 Speed")
+    plt.plot(pid1_time, pid1_speed, label="PID1 Calculated Speed")
+    plt.plot(pid1_time, pid1_control, label="PID1 Control Signal")
+    plt.plot(pid2_time, pid2_speed, label="PID2 Calculated Speed")
+    plt.plot(pid2_time, pid2_control, label="PID2 Control Signal")
+
+    plt.legend(loc='upper left')
 
 
 class MotorController(Node):
@@ -20,17 +54,18 @@ class MotorController(Node):
         self.Kp = 0.8  # TODO tune parameters
         self.Ki = 0.2
         self.Kd = 0.1
+        tracking = True
 
         # Motor setup
-        self.M1 = Motor(9, 10, 11, 12, 13, self.MAX_SPEED)
-        self.M2 = Motor(14, 15, 16, 17, 18, self.MAX_SPEED)
+        self.M1 = Motor(9, 10, 11, 12, 13, self.MAX_SPEED, tracking_name="M1" if tracking else None)
+        self.M2 = Motor(14, 15, 16, 17, 18, self.MAX_SPEED, tracking_name="M2" if tracking else None)
         self.left_rpm = 0
         self.right_rpm = 0
 
         # PID setup
         timer_period = 0.1  # s TODO find best period
-        self.PID1 = PID(self.M1, timer_period, kp=self.Kp, kd=self.Kd, ki=self.Ki)
-        self.PID2 = PID(self.M2, timer_period, kp=self.Kp, kd=self.Kd, ki=self.Ki)
+        self.PID1 = PID(self.M1, timer_period, kp=self.Kp, kd=self.Kd, ki=self.Ki, tracking_name="PID1" if tracking else None)
+        self.PID2 = PID(self.M2, timer_period, kp=self.Kp, kd=self.Kd, ki=self.Ki, tracking_name="PID2" if tracking else None)
         self.timer = self.create_timer(timer_period, self.PID_controller)
 
         # Subscribers and publishers
@@ -40,6 +75,11 @@ class MotorController(Node):
             self.speed_callback,
             10
         )
+
+        if tracking:
+            ani = FuncAnimation(plt.gcf(), speed_monitor, timer_period * 1000)
+            plt.ion()
+            plt.show(block=False)
 
     def speed_callback(self, msg):
         speed = msg.dist * self.MAX_SPEED
