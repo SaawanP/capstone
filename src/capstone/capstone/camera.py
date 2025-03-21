8fa1308eed7dcef357856b8894ca5eaef46e7ca9
Varun Shroff <vshroff@uwaterloo.ca>
2025-03-21 01:06:47 -0400

    test

--- src/capstone/capstone/camera.py
+++ src/capstone/capstone/camera.py
@@ -336,30 +336,30 @@ def main(args=None):
     try:
         GPIO.setmode(GPIO.BCM)
         camera = Camera()
-        with dai.Device(camera.pipeline) as device:
+        with dai.Device() as device:
             camera.get_logger().info("Device connected")
             q_RGB = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
             # q_detections = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
             # q_pointcloud = device.getOutputQueue(name="pcl", maxSize=4, blocking=False)
 
-            while camera.running:
-                in_rgb = q_RGB.get()
-                # in_detections = q_detections.get()
-                # in_pointcloud = q_pointcloud.get()
+            # while camera.running:
+            #     in_rgb = q_RGB.get()
+            #     # in_detections = q_detections.get()
+            #     # in_pointcloud = q_pointcloud.get()
 
-                frame = in_rgb.getCvFrame()
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            #     frame = in_rgb.getCvFrame()
+            #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-                # detections = in_detections.detections
-                # for detection in detections:
-                #     frame = camera.broadcast_defect(detection, frame)
+            #     # detections = in_detections.detections
+            #     # for detection in detections:
+            #     #     frame = camera.broadcast_defect(detection, frame)
 
-                camera.broadcast_frame(frame)
+            #     camera.broadcast_frame(frame)
 
-                # points = in_pointcloud.getPoints()
-                # camera.broadcast_pointcloud_frame(points, frame)
+            #     # points = in_pointcloud.getPoints()
+            #     # camera.broadcast_pointcloud_frame(points, frame)
 
-                rclpy.spin_once(camera)
+            #     rclpy.spin_once(camera)
     finally:
         camera.destroy_node()
         rclpy.shutdown()
